0e8be2c07d3c04c2fec9bfdea7ce046c0dafd0e8
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2022-11-17 19:39:33 +0000

    updated detection to print coords of landmarks

--- face-detection.py
+++ face-detection.py
@@ -20,9 +20,38 @@ with detection.FaceDetection(model_selection = 0,min_detection_confidence=0.5) a
         # Draw the face detection annotations on the frame.
         frame.flags.writeable = True
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
         if results.detections:
-            for detection in results.detections:
-                drawing.draw_detection(frame, detection)
+            print('Nose tip:')
+            print(detection.get_key_point(
+                results.detections[0], detection.FaceKeyPoint.NOSE_TIP))
+            drawing.draw_detection(frame, results.detections[0])
+
+            print('\n Left Ear:')
+            print(detection.get_key_point(
+                results.detections[0], detection.FaceKeyPoint.LEFT_EAR_TRAGION))
+            drawing.draw_detection(frame, results.detections[0])
+
+            print('\n Right Ear:')
+            print(detection.get_key_point(
+                results.detections[0], detection.FaceKeyPoint.RIGHT_EAR_TRAGION))
+            drawing.draw_detection(frame, results.detections[0])
+
+            print('\n Left Eye:')
+            print(detection.get_key_point(
+                results.detections[0], detection.FaceKeyPoint.LEFT_EYE))
+            drawing.draw_detection(frame, results.detections[0])
+
+            print('\n Right Eye:')
+            print(detection.get_key_point(
+                results.detections[0], detection.FaceKeyPoint.RIGHT_EYE))
+            drawing.draw_detection(frame, results.detections[0])
+
+            print('\n Mouth:')
+            print(detection.get_key_point(
+                results.detections[0], detection.FaceKeyPoint.MOUTH_CENTER))
+            drawing.draw_detection(frame, results.detections[0])
+            
         # Flip the frame horizontally for a selfie-view display.
         cv2.imshow('face detection', cv2.flip(frame, 1))
         if cv2.waitKey(5) & 0xFF == ord('q'):
