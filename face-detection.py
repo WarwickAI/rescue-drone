3dc6844862ba2497ef6ef6a6f59d4acf15200dcd
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2022-11-24 17:12:02 +0000

    added reigon detecton

--- face-detection.py
+++ face-detection.py
@@ -17,42 +17,59 @@ with detection.FaceDetection(model_selection = 0,min_detection_confidence=0.5) a
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = faceDetection.process(frame)
 
-        # Draw the face detection annotations on the frame.
+        # Set the frane as writable and convert to normal colour
         frame.flags.writeable = True
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-        if results.detections:
-            print('Nose tip:')
-            print(detection.get_key_point(
-                results.detections[0], detection.FaceKeyPoint.NOSE_TIP))
-            drawing.draw_detection(frame, results.detections[0])
+        if results.detections: #if face detected
+            drawing.draw_detection(frame, results.detections[0]) #draw detections if any
 
-            print('\n Left Ear:')
-            print(detection.get_key_point(
-                results.detections[0], detection.FaceKeyPoint.LEFT_EAR_TRAGION))
-            drawing.draw_detection(frame, results.detections[0])
+            noseX = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.NOSE_TIP).x #get coords for face points
+            noseY = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.NOSE_TIP).y
 
-            print('\n Right Ear:')
-            print(detection.get_key_point(
-                results.detections[0], detection.FaceKeyPoint.RIGHT_EAR_TRAGION))
-            drawing.draw_detection(frame, results.detections[0])
+            lEarX = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.LEFT_EAR_TRAGION).x
+            lEarY = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.LEFT_EAR_TRAGION).y
 
-            print('\n Left Eye:')
-            print(detection.get_key_point(
-                results.detections[0], detection.FaceKeyPoint.LEFT_EYE))
-            drawing.draw_detection(frame, results.detections[0])
+            rEarX = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.RIGHT_EAR_TRAGION).x
+            rEarY = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.RIGHT_EAR_TRAGION).y
 
-            print('\n Right Eye:')
-            print(detection.get_key_point(
-                results.detections[0], detection.FaceKeyPoint.RIGHT_EYE))
-            drawing.draw_detection(frame, results.detections[0])
+            lEyeX = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.LEFT_EYE).x
+            lEyeY = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.LEFT_EYE).y
 
-            print('\n Mouth:')
-            print(detection.get_key_point(
-                results.detections[0], detection.FaceKeyPoint.MOUTH_CENTER))
-            drawing.draw_detection(frame, results.detections[0])
+            rEyeX = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.RIGHT_EYE).x
+            rEyeY = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.RIGHT_EYE).y
+
+            mouthX = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.MOUTH_CENTER).x
+            mouthY = detection.get_key_point(results.detections[0], detection.FaceKeyPoint.MOUTH_CENTER).y
+
+            faceX = (noseX + lEarX + rEarX + lEyeX + rEyeX + mouthX) / 6 #get overall face coords by averaging face point coords
+            faceY = (noseX + lEarY + rEarY + lEyeY + rEyeY + mouthY) / 6
+            cv2.line(frame, (int(faceX * frame.shape[1]), frame.shape[0]), (int(faceX * frame.shape[1]), 0), (255,0,0), 2) #draws lines representing these coords
+            cv2.line(frame, (frame.shape[1], int(faceY * frame.shape[0])), (0, int(faceY * frame.shape[0])), (255,0,0), 2)
+
+            if faceX < 0.33333: #draw a rectangle based on which third the face is in
+                if faceY < 0.33333: #top right
+                    cv2.rectangle(frame, (0,0), (int(frame.shape[1] * 0.33333), int(frame.shape[0] * 0.33333)), (0, 255, 0), 2)
+                elif faceY > 0.66666: #bottom right
+                    cv2.rectangle(frame, (0, frame.shape[0]), (int(frame.shape[1] * 0.33333), int(frame.shape[0] * 0.66666)), (0, 255, 0), 2)
+                else: #middle right
+                    cv2.rectangle(frame, (0,int(frame.shape[0]*0.66666)), (int(frame.shape[1] * 0.33333), int(frame.shape[0] * 0.33333)), (0, 255, 0), 2)
+            elif faceX > 0.66666:
+                if faceY < 0.33333: #top left
+                    cv2.rectangle(frame, (int(frame.shape[1]),0), (int(frame.shape[1] * 0.66666), int(frame.shape[0] * 0.33333)), (0, 255, 0), 2)
+                elif faceY > 0.66666: #bottom left
+                    cv2.rectangle(frame, (int(frame.shape[1]), frame.shape[0]), (int(frame.shape[1] * 0.66666), int(frame.shape[0] * 0.66666)), (0, 255, 0), 2)
+                else: #middle left
+                    cv2.rectangle(frame, (int(frame.shape[1]),int(frame.shape[0]*0.66666)), (int(frame.shape[1] * 0.66666), int(frame.shape[0] * 0.33333)), (0, 255, 0), 2)
+            else:
+                if faceY < 0.33333: #top middle
+                    cv2.rectangle(frame, (int(frame.shape[1] * 0.66666),0), (int(frame.shape[1] * 0.33333), int(frame.shape[0] * 0.33333)), (0, 255, 0), 2)
+                elif faceY > 0.66666: #bottom middle
+                    cv2.rectangle(frame, (int(frame.shape[1] * 0.66666), frame.shape[0]), (int(frame.shape[1] * 0.33333), int(frame.shape[0] * 0.66666)), (0, 255, 0), 2)
+                else: #middle middle
+                    cv2.rectangle(frame, (int(frame.shape[1] * 0.66666),int(frame.shape[0]*0.66666)), (int(frame.shape[1] * 0.33333), int(frame.shape[0] * 0.33333)), (0, 255, 0), 2)
             
-        # Flip the frame horizontally for a selfie-view display.
+        # Flip the frame
         cv2.imshow('face detection', cv2.flip(frame, 1))
         if cv2.waitKey(5) & 0xFF == ord('q'):
             break
